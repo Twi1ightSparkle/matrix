b8805930a7ba2aecfd9024c135b589b4964e494e
Twilight Sparkle <19155609+Twi1ightSparkle@users.noreply.github.com>
2020-12-07 21:33:43 +0000

    Initial commit

---
+++ mass_upgrade_rooms.py
@@ -0,0 +1,101 @@
+# Import modules
+import json
+import logging
+import os
+import requests
+import time
+from tqdm import tqdm
+from mass_upgrade_rooms_config import Config
+
+
+if __name__ == "__main__":
+    # Configure logging
+    log_file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "mass_upgrade_rooms.log")
+    log_level = getattr(logging, Config.log_level.upper())
+    logging.basicConfig(
+        filename=log_file_path,
+        format="%(asctime)s %(levelname)s %(message)s",
+        level=log_level
+    )
+
+    # Headers to sent to the rest API
+    url = "%s/_matrix/client/r0/createRoom" % Config.homeserver_delegated_url
+    headers = {
+        "Authorization": "Bearer %s" % Config.admin_token,
+        "Content-Type": "application/json"
+    }
+    logging.debug("url: %s" % url)
+
+    # Loop over all rooms
+    logging.debug("Config.rooms: %s" %Config.rooms)
+    for room in tqdm(Config.rooms):
+        room_id, room_alias = room
+
+        # Clean Room ID
+        room_id_safe = room_id.replace("!", "%21").replace(":", "%3A")
+        logging.debug("room_id: %s" %room_id)
+
+        upgrade_url = "%s/_matrix/client/r0/rooms/%s/upgrade" %(Config.homeserver_delegated_url, room_id_safe)
+        data = {"new_version": str(Config.new_room_version)}
+        logging.debug("upgrade_url: %s - data: %s" % (upgrade_url, data))
+
+        # Upgrade the room
+        try:
+            upgrade_result = requests.request(
+                method="POST",
+                url=upgrade_url,
+                headers=headers,
+                data=json.dumps(data)
+            )
+        except requests.exceptions.ConnectionError as err:
+            logging.error("%s - %s" % (room_alias, err))
+            exit()
+
+        # If successful upgrade
+        if upgrade_result.status_code == 200:
+            upgrade_result = upgrade_result.json()
+            logging.debug(upgrade_result)
+            logging.info("Room %s successfully upgraded. Old Room ID: %s - New Room ID: %s" % (room_alias, room_id, upgrade_result["replacement_room"]))
+
+            # Send a message to the old room
+            message_url = "%s/_matrix/client/r0/rooms/%s/send/m.room.message" %(Config.homeserver_delegated_url, room_id_safe)
+            data = {"msgtype": "m.text", "body": Config.message_template(room_id, room_alias)}
+            logging.debug("message_url: %s - data: %s" %(message_url, data))
+            try:
+                message_result = requests.request(
+                    method="POST",
+                    url=message_url,
+                    headers=headers,
+                    data=json.dumps(data)
+                )
+            except requests.exceptions.ConnectionError as err:
+                logging.error("%s - %s" % (room_alias, err))
+                exit()
+
+            # If sending message was successful
+            if message_result.status_code == 200:
+                logging.debug("message_result: %s" %message_result.json())
+                logging.info("Successfully messaged room %s" %room_id)
+            else:
+                try: # Convert result to a dictionary
+                    message_result = message_result.json()
+                except json.decoder.JSONDecodeError as err: # If not JSON
+                    logging.error("%s - %s - %s" % (room_id, room_alias, err))
+                else: # If JSON
+                    message_result["room_id"] = room_id
+                    message_result["room_alias"] = room_alias
+                    logging.error(message_result)
+
+        # If error on upgrade
+        else:
+            try: # Convert result to a dictionary
+                upgrade_result = upgrade_result.json()
+            except json.decoder.JSONDecodeError as err: # If not JSON
+                logging.error("%s - %s - %s" % (room_id, room_alias, err))
+            else: # If JSON
+                upgrade_result["room_id"] = room_id
+                upgrade_result["room_alias"] = room_alias
+                logging.error(upgrade_result)
+
+        # Wait a minute to not get rate limited, and to not overwhelm the server
+        time.sleep(60)
---
+++ mass_upgrade_rooms_config.example.py
@@ -0,0 +1,31 @@
+class Config():
+    
+    # Log level. INFO or DEBUG
+    log_level = "INFO"
+
+    # Homeserver URLs
+    homeserver_delegated_url = "https://matrix.example.com"
+
+    # User to make the room upgrade with, this user must have the correct permissions in the room. Usually 100 / Admin
+    admin_token = "MDA..."
+
+    # Upgrade to this room version
+    new_room_version = 6
+
+    # Template for message to send to old room. To cover for clients that does not support the tombstone event
+    def message_template(new_room_id, room_alias):
+        return "This room has been upgraded. The alias %s has been moved to the new room. The new room ID is %s" % (room_alias, new_room_id)
+
+    # List of rooms to process. Alias can be an empty string, only used for human readable log
+    rooms = [
+        ["!rCjhWzWtIAoAPmzKuQ:example.com", "#room0:example.com"],
+        ["!BjjosTZswnbWcqaEJJ:example.com", "#room1:example.com"],
+        ["!LQmdtmUtMLEBAGvMQW:example.com", "#room2:example.com"],
+        ["!jJHFQJekwMQgZhMqKa:example.com", "#room3:example.com"],
+        ["!DQzQAXbRYIwAMCtbCu:example.com", "#room4:example.com"],
+        ["!xOuGeTUOHHITrvdcyD:example.com", "#room5:example.com"],
+        ["!UANpAeMRFrwHNlqjxC:example.com", "#room6:example.com"],
+        ["!XCLYvAajKdRukUsIph:example.com", "#room7:example.com"],
+        ["!PTTqFpUfhkMpSYeLLQ:example.com", "#room8:example.com"],
+        ["!YEOvNWPlmnvmvkFJVP:example.com", "#room9:example.com"],
+    ]
